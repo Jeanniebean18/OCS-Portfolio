6c97e18dbb560fde0277fb058f7ae10c6d8ada35
Jeannie <jeannie@kickpunchcreative.com>
2015-08-11 21:47:37 -0500

    back to squares

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-
-  var workOne=document.getElementById("work-one");
-  var workTwo=document.getElementById("work-two");
+  var work=document.getElementById("work");
+ 
   var workFour=document.getElementById("work-four");
-  var workThree=document.getElementById("work-three");
+
   var workFive=document.getElementById("work-five");
   var workSix=document.getElementById("work-six");
 
@@ -12,28 +11,17 @@
   
   function inView(){
     
-    if(isInViewport(workOne)){
-    workOne.style.visibility="visible";
-    workOne.setAttribute("class","animated zoomIn");
-    workTwo.style.visibility="visible";
-    workTwo.setAttribute("class","animated zoomIn");
-    workThree.style.visibility="visible";
-    workThree.setAttribute("class","animated zoomIn");
-    
-    
-    
-    } else {
-      
-      workOne.setAttribute("class","animated fadeOut");
-     
-      workTwo.setAttribute("class","animated fadeOut");
-      
-      workThree.setAttribute("class","animated fadeOut");
-      // workThree.style.visibility="hidden";
-//        workTwo.style.visibility="hidden";
-//       workOne.style.visibility="hidden";
-      
-    }
+    if(isInViewport(work)){
+ 
+      work.style.visibility="visible";
+      work.setAttribute("class","animated zoomIn");
+
+  } else {
+    work.setAttribute("class","animated fadeOut");
+  
+  
+  }
+
   
 
   if(isInViewport(workFour)){
