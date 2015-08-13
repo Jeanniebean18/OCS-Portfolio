e8b1493e5dbf0a72c78da34e320df3d8b53ab280
Jeannie <jeannie@kickpunchcreative.com>
2015-08-13 10:45:33 -0500

    hover state works

--- js/main.js
+++ js/main.js
@@ -1,62 +1,86 @@
  
   
-  var work=document.getElementById("work");
- 
-  var workFour=document.getElementById("work-four");
+var student=document.getElementById("student_quote");
+var studentTwo=document.getElementById("student_quote_two");
+var workOne=document.getElementById("work-one");
+var workTwo=document.getElementById("work-two");
+var workThree=document.getElementById("work-three");
+var workFour=document.getElementById("work-four");
+var workFive=document.getElementById("work-five");
+var workSix=document.getElementById("work-six");
+var hover_one=document.getElementById("over-work-one");
 
-  var workFive=document.getElementById("work-five");
-  var workSix=document.getElementById("work-six");
+  
+  
+  
+window.onscroll=inView;
+  
+  
+function inView(){
 
   
-  window.onscroll=inView;
-  
-  
-  function inView(){
-    
-    if(isInViewport(work)){
- 
-      work.style.visibility="visible";
-      work.setAttribute("class","animated zoomIn");
+  if(isInViewport(workOne)){
+
+    workOne.style.visibility="visible";
+    workOne.setAttribute("class","animated zoomIn");
+    workTwo.style.visibility="visible";
+    workTwo.setAttribute("class","animated delay1 zoomIn");
+    workThree.style.visibility="visible";
+    workThree.setAttribute("class","animated delay2 zoomIn");
 
   } else {
-    work.setAttribute("class","animated fadeOut");
-  
-  
+    workOne.setAttribute("class","animated fadeOut");
+    workTwo.setAttribute("class","animated fadeOut");
+    workThree.setAttribute("class","animated fadeOut");
   }
 
+    if(isInViewport(student)){
+
+      student.style.visibility="visible";
+      student.setAttribute("class","animated zoomIn");
+
+    } else {
+      student.setAttribute("class","animated fadeOut");
+
+
+    }
+    
+    
+    
+    
+    if(isInViewport(workFour)){
+
+      workFour.style.visibility="visible";
+      workFour.setAttribute("class","animated zoomIn");
+      workFive.style.visibility="visible";
+      workFive.setAttribute("class","animated delay1 zoomIn");
+      workSix.style.visibility="visible";
+      workSix.setAttribute("class","animated delay2 zoomIn");
   
 
-  if(isInViewport(workFour)){
- 
-    workFour.style.visibility="visible";
-    workFour.setAttribute("class","animated zoomIn");
+    } else {
+      workFour.setAttribute("class","animated fadeOut");
+      workFive.setAttribute("class","animated fadeOut");
+      workSix.setAttribute("class","animated fadeOut");
+     
+    }
 
-} else {
-  workFour.setAttribute("class","animated fadeOut");
-  
-  
-}
+      if(isInViewport(studentTwo)){
 
-  if(isInViewport(workFive)){
- 
-    workFive.style.visibility="visible";
-    workFive.setAttribute("class","animated zoomIn");
-    workSix.style.visibility="visible";
-    workSix.setAttribute("class","animated zoomIn");
+        studentTwo.style.visibility="visible";
+        studentTwo.setAttribute("class","animated zoomIn");
 
-} else {
-  workFive.setAttribute("class","animated fadeOut");
-  workSix.setAttribute("class","animated fadeOut");
-  
-  
-}
-  
-  
+      } else {
+        studentTwo.setAttribute("class","animated fadeOut");
+
+
+      }
+
 
 };
   
   
   
-  var isInViewport=function(elem){var distance=elem.getBoundingClientRect();return(distance.top>=0&&distance.left>=0&&distance.bottom<=(window.innerHeight||document.documentElement.clientHeight)&&distance.right<=(window.innerWidth||document.documentElement.clientWidth));};
+var isInViewport=function(elem){var distance=elem.getBoundingClientRect();return(distance.top>=0&&distance.left>=0&&distance.bottom<=(window.innerHeight||document.documentElement.clientHeight)&&distance.right<=(window.innerWidth||document.documentElement.clientWidth));};
   
-  
+
