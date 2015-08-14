523120094d5d0bad7d42b40292a5c0e2f4a2ee60
Jeannie <jeannie@kickpunchcreative.com>
2015-08-14 12:39:40 -0500

    star rolls in and out on scroll

--- public/js/main.js
+++ public/js/main.js
@@ -3,128 +3,62 @@ var studentTwo=document.getElementById("student_quote_two");
 var workFour=document.getElementById("work-four");
 var workFive=document.getElementById("work-five");
 var workSix=document.getElementById("work-six");
+var star=document.getElementById("star");
 
 
-window.onscroll=inView;
+
+
+
+
+  window.onscroll=inView;
   
   
-function inView(){
-
-  if (document.documentElement.clientWidth  >= 600) {
+    function inView(){
+  // roll out of top star
+      if (window.pageYOffset>75) {
+        star.setAttribute("class","animated rollOut star2");
+      } 
       
-
+      if (window.pageYOffset<75) {
+        star.setAttribute("class","animated rollIn star2");
+      } 
       
-    if(isInViewport(student)){
-
-      student.style.visibility="visible";
-      student.setAttribute("class","animated zoomIn");
-
-    } 
     
-    if(isInViewport(workFour)){
+// student work and quotes in viewport in viewport
+      if (document.documentElement.clientWidth  >= 600) {
+      
+     
+        
+        if(isInViewport(student)){
 
-      workFour.style.visibility="visible";
-      workFour.setAttribute("class","animated zoomIn");
-      workFive.style.visibility="visible";
-      workFive.setAttribute("class","animated delay1 zoomIn");
-      workSix.style.visibility="visible";
-      workSix.setAttribute("class","animated delay2 zoomIn");
-  
+          student.style.visibility="visible";
+          student.setAttribute("class","animated zoomIn");
 
-    } 
+        } 
+    
+        if(isInViewport(workFour)){
 
-    if(isInViewport(studentTwo)){
+          workFour.style.visibility="visible";
+          workFour.setAttribute("class","animated zoomIn");
+          workFive.style.visibility="visible";
+          workFive.setAttribute("class","animated delay1 zoomIn");
+          workSix.style.visibility="visible";
+          workSix.setAttribute("class","animated delay2 zoomIn");
+        } 
 
-      studentTwo.style.visibility="visible";
-      studentTwo.setAttribute("class","animated zoomIn");
-    } 
-  }
+        if(isInViewport(studentTwo)){
 
-
-};
-  
-  
-  
-var isInViewport=function(elem){var distance=elem.getBoundingClientRect();return(distance.top>=0&&distance.left>=0&&distance.bottom<=(window.innerHeight||document.documentElement.clientHeight)&&distance.right<=(window.innerWidth||document.documentElement.clientWidth));};
-  
-
-
-(function() {
-
-  'use strict';
-
-  // Feature Test
-  if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
-
-    // Function to animate the scroll
-    var smoothScroll = function (anchor, duration) {
-
-      // Calculate how far and how fast to scroll
-      var startLocation = window.pageYOffset;
-      var endLocation = anchor.offsetTop;
-      var distance = endLocation - startLocation;
-      var increments = distance/(duration/16);
-      var stopAnimation;
-
-      // Scroll the page by an increment, and check if it's time to stop
-      var animateScroll = function () {
-        window.scrollBy(0, increments);
-        stopAnimation();
-      };
-
-      // If scrolling down
-      if ( increments >= 0 ) {
-        // Stop animation when you reach the anchor OR the bottom of the page
-        stopAnimation = function () {
-          var travelled = window.pageYOffset;
-          if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
-            clearInterval(runAnimation);
-          }
-        };
-      }
-      // If scrolling up
-      else {
-        // Stop animation when you reach the anchor OR the top of the page
-        stopAnimation = function () {
-          var travelled = window.pageYOffset;
-          if ( travelled <= (endLocation || 0) ) {
-            clearInterval(runAnimation);
-          }
-        };
+          studentTwo.style.visibility="visible";
+          studentTwo.setAttribute("class","animated zoomIn");
+        } 
       }
 
-      // Loop the animation function
-      var runAnimation = setInterval(animateScroll, 5);
-       
+
     };
+  
+  
+  
+    var isInViewport=function(elem){var distance=elem.getBoundingClientRect();return(distance.top>=0&&distance.left>=0&&distance.bottom<=(window.innerHeight||document.documentElement.clientHeight)&&distance.right<=(window.innerWidth||document.documentElement.clientWidth));};
+  
+  
 
-    // Define smooth scroll links
-    var scrollToggle = document.querySelectorAll('.scroll');
-
-    // For each smooth scroll link
-    [].forEach.call(scrollToggle, function (toggle) {
-
-      // When the smooth scroll link is clicked
-      toggle.addEventListener('click', function(e) {
-
-        // Prevent the default link behavior
-        e.preventDefault();
-
-        // Get anchor link and calculate distance from the top
-        var dataID = toggle.getAttribute('href');
-        var dataTarget = document.querySelector(dataID);
-        var dataSpeed = toggle.getAttribute('data-speed');
-
-        // If the anchor exists
-        if (dataTarget) {
-          // Scroll to the anchor
-          smoothScroll(dataTarget, dataSpeed || 500);
-        }
-
-      }, false);
-
-    });
-
-  }
-
-})();
